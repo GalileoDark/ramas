6627b99b3aad8865fe181b1006db6f4b66284df5
GalileoDark <raulnavidadherrera1996@gmail.com>
2024-09-27 17:32:04 +0200

    ampliacion del hola mundo

--- holamundo.java
+++ holamundo.java
@@ -1,7 +1,8 @@
 public class HolaMundo {
 
   public static void main(String[] args) {
-    System.out.println("Hola Mundo");
+    System.out.println("Hola Mundo cruel");
+	System.out.println("Adios mundo cruel");
   }
 
 }
